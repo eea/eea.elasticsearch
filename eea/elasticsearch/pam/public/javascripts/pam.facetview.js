509e39a05916a5cc9938251b8981777370123be5
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2013-09-16 18:06:53 +0300

    Refs #15669; Customizations for search page

--- eea/elasticsearch/pam/public/javascripts/pam.facetview.js
+++ eea/elasticsearch/pam/public/javascripts/pam.facetview.js
@@ -1,9 +1,14 @@
+function addheaders(){
+    $("#facetview_results").append("<thead><tr><th>Country</th><th>Sector</th><th>Projection Scenario</th><th>Name</th><th>Type</th><th>GHG</th><th>Status</th><th></th></tr></thead>");
+}
 jQuery(document).ready(function($) {
     $('.facet-view-simple').facetview({
         search_url: 'http://centaurus.eea.europa.eu/elasticsearch/rdfdata/_search?',
         search_index: 'elasticsearch',
+        initial_search: false,
+        post_search_callback: addheaders,
         facets: [
-                        {'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Country_with_EU', 'display': 'Country'},
+            {'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Country_with_EU', 'display': 'Country', 'size':'50', 'order': 'term'},
             {'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Sector_List', 'display': 'Sector'},
             {'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Policy_Type_List', 'display': 'Policy Type'},
             {'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#GHG_affected', 'display': 'GHG Affected'},
@@ -15,24 +20,41 @@ jQuery(document).ready(function($) {
             [
                 {
                     'field': "http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Country",
+                    'post': '</td>',
                 },
                 {
-                    'field': "http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Sector",
+                    'pre': '<td>',
+                    'field': "http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Sector_List",
+                    'post': '</td>',
+
                 },
                 {
+                    'pre': '<td>',
                     'field': "http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Projections_scenario_in_which_the_PAM_is_included",
+                    'post': '</td>',
                 },
                 {
+                    'pre': '<td>',
                     'field': "http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Name_of_PAM",
+                    'post': '</td>',
                 },
                 {
-                    'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Policy_Type_List'
+                    'pre': '<td>',
+                    'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Policy_Type_List',
+                    'post': '</td>',
                 },
                 {
-                    'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#GHG_affected'
+                    'pre': '<td>',
+                    'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#GHG_affected',
+                    'post': '</td>',
                 },
                 {
-                    'pre': '<a href="/details?pamid=',
+                    'pre': '<td>',
+                    'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Status',
+                    'post': '</td>',
+                },
+                {
+                    'pre': '<td><a href="/details?pamid=',
                     'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#PAMID',
                     'post': '">Details</a>',
                 },
