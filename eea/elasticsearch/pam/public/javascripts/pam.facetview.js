025be94b42e086ca2b75048d8a4d6eed99fbb00d
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2013-09-23 15:09:01 +0300

    Refs #15669; Fixed js problems; Added jquery tools; Added formatter script for GHG fields

--- eea/elasticsearch/pam/public/javascripts/pam.facetview.js
+++ eea/elasticsearch/pam/public/javascripts/pam.facetview.js
@@ -1,12 +1,44 @@
-function addheaders(){
+function replaceNumbers(){
+    var possibleContainers = ['a', 'td', 'th'];
+    var chemsMapping = {'CH4':'CH<sub>4</sub>',
+                        'CO2':'CO<sub>2</sub>',
+                        'N2O':'N<sub>2</sub>O',
+                        'SF6':'SF<sub>6</sub>'};
+    jQuery.each(possibleContainers, function(idx, container){
+        var elems = jQuery(container);
+        jQuery.each(elems, function(idx, elem){
+            if ((jQuery(elem).children().length === 0) || (container === 'a')){
+                var shouldReplace = false
+                var replacedText = jQuery(elem).html()
+                jQuery.each(chemsMapping, function(key, value){
+                    if (replacedText.indexOf(key) !== -1){
+                        replacedText = replacedText.replace(key, value);
+                        shouldReplace = true;
+                    }
+                });
+                if (shouldReplace){
+                    jQuery(elem).html(replacedText);
+                }
+            }
+        });
+    });
+}
+
+function addHeaders(){
     $("#facetview_results").append("<thead><tr><th>Country</th><th>Sector</th><th>Projection Scenario</th><th>Name</th><th>Type</th><th>GHG</th><th>Status</th></tr></thead>");
 }
+
+function viewReady(){
+    replaceNumbers();
+    addHeaders();
+}
+
 jQuery(document).ready(function($) {
     $('.facet-view-simple').facetview({
         search_url: 'http://centaurus.eea.europa.eu/elasticsearch/pamdata/resources/_search?',
         search_index: 'elasticsearch',
         initial_search: false,
-        post_search_callback: addheaders,
+        post_search_callback: viewReady,
         pushstate : false,
         facets: [
             {'field':'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#Country_with_EU', 'display': 'Country', 'size':'50', 'order': 'term'},
@@ -72,4 +104,5 @@ jQuery(document).ready(function($) {
             'pamid': {'term':{'http://semantic.eea.europa.eu/home/szabozo0/pam1.csv#PAMID':'tra'}}
         }*/
     });
+    replaceNumbers(); 
 });
