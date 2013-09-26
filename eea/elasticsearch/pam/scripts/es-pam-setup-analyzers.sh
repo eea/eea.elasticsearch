6ae38fcc98f9e72becfb6fc66b7689258b911335
Iulia Pasov <iulia.pasov@eaudeweb.ro>
2013-09-26 17:45:41 +0300

    Updated scripts according to new specifications refs #15669

--- eea/elasticsearch/pam/scripts/es-pam-setup-analyzers.sh
+++ eea/elasticsearch/pam/scripts/es-pam-setup-analyzers.sh
@@ -1,60 +1,62 @@
-curl -XPUT 'http://centaurus.eea.europa.eu/elasticsearch/pamdata' -d '{
-   "settings" : {
-      "analysis" : {
-         "analyzer" : {
-            "none" : {
-               "type" : "keyword" 
-         },
-            "coma" : {
-               "type" : "pattern",
-        "lowercase" : false,
-        "pattern" : ", " 
+curl -u eea:eea -XPUT 'http://centaurus.eea.europa.eu/elasticsearch/pamdata' -d '{
+  "settings" : {
+    "analysis" : {
+      "analyzer" : {
+        "none" : {
+          "type" : "keyword"
+        },
+          "coma" : {
+            "type" : "pattern",
+            "lowercase" : false,
+            "pattern" : ", "
+          }
+      }
+    }
+  },
+  "mappings" : {
+    "resources" : {
+      "properties" : {
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Country" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#PAMID"  : {
+          "type" : "string",
+          "analyzer" : "none"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Policy_Type_List" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#GHG_affected" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Sector_List" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Projections_scenario_in_which_the_PAM_is_included" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Link_to_EU_Emissions_Trading_Scheme_EU_ETS" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Implementing_entities_List" : {
+          "type" : "string",
+          "analyzer" : "coma"
+        },
+        "http://semantic.eea.europa.eu/project/pam/pam.csv#Primary_EU_policy_responsible_objectives" : {
+          "type" : "string",
+          "analyzer" : "none"
+        },
+		"http://semantic.eea.europa.eu/project/pam/pam.csv#Related_EU_policies" : {
+          "type" : "string",
+          "analyzer" : "coma"
         }
-     }
       }
-   },
-   "mappings" : {
-      "resources" : {
-         "properties" : { 
-        "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Country" : {
-           "type" : "string", 
-           "analyzer" : "coma" 
-        },
-        "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#PAMID"  : {
-           "type" : "string",
-           "analyzer" : "none" 
-        },
-        "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Policy_Type_List" : {
-           "type" : "string", 
-           "analyzer" : "coma" 
-        },
-        "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#GHG_affected" : {
-           "type" : "string", 
-           "analyzer" : "coma" 
-        },
-        "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Sector_List" : {
-           "type" : "string", 
-           "analyzer" : "coma" 
-        },
-            "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Projections_scenario_in_which_the_PAM_is_included" : {
-               "type" : "string",
-               "analyzer" : "coma" 
-        },
-            "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Link_to_EU_Emissions_Trading_Scheme_EU_ETS" : {
-               "type" : "string",
-               "analyzer" : "coma" 
-        },
-            "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Implementing_entities_List" : {
-               "type" : "string",
-               "analyzer" : "coma" 
-       },
-            "http://semantic.eea.europa.eu/project/pam/pam_v10.csv#Primary_EU_policy_responsible_objectives" : {
-               "type" : "string",
-               "analyzer" : "none" 
-       }
-
-     }
-      }
-   }
+    }
+  }
 }'
-
