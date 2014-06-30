9b3fc6b3e4faf77ed564dde0321e1f6e8f2b9f71
Iulia Pasov <iulia.pasov@gmail.com>
2014-06-30 11:41:04 +0300

    Added all rivers to delete

--- webapps/eea_search/scripts/es-remove-river.sh
+++ webapps/eea_search/scripts/es-remove-river.sh
@@ -1,2 +1,8 @@
+curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search0'
 curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search1'
 curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search2'
+curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search3'
+curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search4'
+curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search5'
+curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search6'
+curl -u eea:eea -XDELETE 'http://centaurus-dev.eea.europa.eu/elasticsearch/_river/search7'
