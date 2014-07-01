5b85152fdc371da1c2f676822a000f91f84dc252
Iulia Pasov <iulia.pasov@gmail.com>
2014-07-01 16:44:58 +0300

    Checked js with linter

--- eea/elasticsearch/js/settings.js
+++ eea/elasticsearch/js/settings.js
@@ -2,28 +2,29 @@ $(function($) {
 
   window.hide_unused_options = function(blackList, whiteList) {
     var filters = $('a.facetview_filterchoice');
-    for (filter in filters) {
+    for (var filter in filters) {
       var thisFilter = filters[filter];
-      var value = undefined;
+      var value;
       if (thisFilter.href) {
-        value = thisFilter.href.substring(thisFilter.href.lastIndexOf('/') + 1, thisFilter.href.length);
+        value = thisFilter.href.substring(
+          thisFilter.href.lastIndexOf('/') + 1,
+          thisFilter.href.length);
       }
       if (!whiteList.isEmptyObject) {
         var toKeep = whiteList[thisFilter.rel];
-        if(toKeep && toKeep.indexOf(value) == -1) {
+        if (toKeep && toKeep.indexOf(value) === -1) {
           hidden = $(thisFilter.parentNode).remove();
         }
 
       } else {
         var toHide = blackList[thisFilter.rel];
-        if (toHide == undefined) {
+        if (toHide === undefined) {
           continue;
         }
-        if(toHide.indexOf(value) >=0) {
+        if (toHide.indexOf(value) >= 0) {
           $(thisFilter.parentNode).remove();
-        }  
+        }
       }
-      
     }
   };
 
@@ -37,19 +38,28 @@ $(function($) {
 
       var button = '<button class="btn btn-small btn-lg" data-toggle="modal"' +
           'data-target="#myModal">' + 'Embed' + ' </button>';
-      var popup = '<div class="modal fade" id="myModal" tabindex="-1"' +
-            'role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">' +
-            '<div class="modal-dialog">' + '<div class="modal-content">' +
-            '<div class="modal-header">' + '<button type="button" class="close"' +
-            'data-dismiss="modal" aria-hidden="true">&times;</button>' +
-            ' <h4 class="modal-title" id="myModalLabel">Embed code</h4>' +
-            '<div class="modal-body"> <textarea style="width:95%" rows="3">' +
-            '<iframe width="' + width + '" height="' + height + '" src="' + url +
-            '"></iframe></textarea></div></div></div></div></div>';
+      var popup = [
+        '<div class="modal fade" id="myModal" tabindex="-1"',
+        'role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">',
+        '<div class="modal-dialog">',
+        '<div class="modal-content">',
+        '<div class="modal-header">',
+        '<button type="button" class="close"',
+        'data-dismiss="modal" aria-hidden="true">&times;</button> ',
+        '<h4 class="modal-title" id="myModalLabel">Embed code</h4>',
+        '<div class="modal-body"> <textarea style="width:95%" rows="3">',
+        '<iframe width="',
+        width,
+        '" height="',
+        height,
+        '" src="',
+        url,
+        '"></iframe></textarea></div></div></div></div></div>'
+      ].join('');
 
       $('.facet-embed').append(button + popup);
 
     }
-  }
+  };
 
 });
