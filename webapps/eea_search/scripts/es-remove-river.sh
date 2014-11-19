16c378eb82700cb166b42ff13391aa0f4449a314
Iulia Pasov <iulia.pasov@gmail.com>
2014-11-19 16:46:43 +0200

    Fixed deletion script for rivers

--- webapps/eea_search/scripts/es-remove-river.sh
+++ webapps/eea_search/scripts/es-remove-river.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
 curl -u eea:eea -XDELETE $1'/_river/search'
-curl -u eea:eea -XDELETE $1'/_river/search0'
-curl -u eea:eea -XDELETE $1'/_river/search1'
-curl -u eea:eea -XDELETE $1'/_river/search2'
-curl -u eea:eea -XDELETE $1'/_river/search3'
-curl -u eea:eea -XDELETE $1'/_river/search4'
-curl -u eea:eea -XDELETE $1'/_river/search5'
-curl -u eea:eea -XDELETE $1'/_river/search6'
-curl -u eea:eea -XDELETE $1'/_river/search7'
