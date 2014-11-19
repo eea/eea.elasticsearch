3eebbdbaa42dbdf91472ca0ea208e76317b9f480
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2014-11-19 13:08:28 +0200

    Refs #21551; Updated import scripts and config files for new pam

--- webapps/pam/scripts/es-pam-setup-analyzers.sh
+++ webapps/pam/scripts/es-pam-setup-analyzers.sh
@@ -21,39 +21,39 @@ curl -u eea:eea -XPUT 'http://centaurus.eea.europa.eu/elasticsearch/pamdata' -d 
   "mappings" : {
     "resources" : {
       "properties" : {
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Country" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Country" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#PAMID"  : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#PAMID"  : {
           "type" : "string",
           "analyzer" : "none"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Type_of_instrument" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Type_of_instrument" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Affected_GHG" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Affected_GHG" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Targeted_sectors" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Targeted_sectors" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Projection_scenario_in_which_the_PAM_is_included" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Projection_scenario_in_which_the_PAM_is_included" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Link_to_EU_Emissions_Trading_Scheme_ETS" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Link_to_EU_Emissions_Trading_Scheme_ETS" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Type_of_implementing_entities" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Type_of_implementing_entities" : {
           "type" : "string",
           "analyzer" : "coma"
         },
-        "http://semantic.eea.europa.eu/project/pam/pam.csv#Related_EU_policies" : {
+        "http://semantic.eea.europa.eu/project/pam/pam2014.csv#Related_EU_policies" : {
           "type" : "string",
           "analyzer" : "semicolon"
         }
