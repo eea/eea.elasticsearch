9db07bcabde6ca5af1b1c4572ddfb39f727b7b56
Iulia Pasov <iulia.pasov@gmail.com>
2014-11-27 18:14:45 +0200

    Changed iframe function

--- eea/elasticsearch/js/settings.js
+++ eea/elasticsearch/js/settings.js
@@ -1,42 +1,6 @@
 $(function($) {
-
-  function add_iframe() {
-    if (window.embed) {
-      var url = $(location).attr('href');
-      var position = url.indexOf('?');
-      url = url.substring(0, position);
-      var width = $('.facet-embed')[0].offsetWidth;
-      var height = $('.content').height();
-
-      var button = '<button class="btn btn-small btn-lg" data-toggle="modal"' +
-          'data-target="#myModal">' + 'Embed' + ' </button>';
-      var popup = [
-        '<div class="modal fade" id="myModal" tabindex="-1"',
-        'role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">',
-        '<div class="modal-dialog">',
-        '<div class="modal-content">',
-        '<div class="modal-header">',
-        '<button type="button" class="close"',
-        'data-dismiss="modal" aria-hidden="true">&times;</button> ',
-        '<h4 class="modal-title" id="myModalLabel">Embed code</h4>',
-        '<div class="modal-body"> <textarea style="width:95%" rows="3">',
-        '<iframe width="',
-        width,
-        '" height="',
-        height,
-        '" src="',
-        url,
-        '"></iframe></textarea></div></div></div></div></div>'
-      ].join('');
-
-      $('.facet-embed').append(button + popup);
-
-    }
-  }
-
   if (window.hastemplate === undefined) {
     window.hastemplate = true;
-    add_iframe();
     $.get('external_templates/head.html', function(data) {
       $('head').append(data);
     });
--- webapps/eea_search/js/search.js
+++ webapps/eea_search/js/search.js
@@ -130,9 +130,40 @@ $(function($) {
     } else {
       $('.top-pagination').html('');
     }
+  }
 
+  function add_iframe() {
+    if (window.embed) {
+      var url = $(location).attr('href');
+      var position = url.indexOf('?');
+      url = url.substring(0, position);
+      var width = $('.facet-embed')[0].offsetWidth;
+      var height = $('.content').height();
 
+      var button = '<button class="btn btn-small btn-lg" data-toggle="modal"' +
+          'data-target="#myModal">' + 'Embed' + ' </button>';
+      var popup = [
+        '<div class="modal fade" id="myModal" tabindex="-1"',
+        'role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">',
+        '<div class="modal-dialog">',
+        '<div class="modal-content">',
+        '<div class="modal-header">',
+        '<button type="button" class="close"',
+        'data-dismiss="modal" aria-hidden="true">&times;</button> ',
+        '<h4 class="modal-title" id="myModalLabel">Embed code</h4>',
+        '<div class="modal-body"> <textarea style="width:95%" rows="3">',
+        '<iframe width="',
+        width,
+        '" height="',
+        height,
+        '" src="',
+        url,
+        '"></iframe></textarea></div></div></div></div></div>'
+      ].join('');
 
+      $('.facet-embed').append(button + popup);
+
+    }
   }
 
   function display_results() {
@@ -284,6 +315,7 @@ $(function($) {
         display_results();
         hide_unused_options(blackList, whiteList);
         add_EEA_settings();
+        add_iframe();
       },
       linkify: false,
              paging: {
