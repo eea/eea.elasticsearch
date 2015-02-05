d542e71b9627595e25f9c421354fa3757483174c
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-02-05 12:36:23 +0200

    Refs #21931; Query was splitted in smaller queries

--- webapps/aqstats/scripts/createIndex.js
+++ webapps/aqstats/scripts/createIndex.js
@@ -1,11 +1,27 @@
 var api = require('../../common/lib/esAPI.js');
 
+var queryTemplate = "SELECT ?s ?p ?o WHERE" +
+                        "{"+
+                            "{ ?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o FILTER(isLiteral(?o))}"+
+                            "UNION"+
+                            "{?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 ."+
+                                "?o1 <http://www.w3.org/2000/01/rdf-schema#label> ?o  }"+
+                        "<filter>"+
+                        "}";
+
+var filterTemplate = "FILTER (str(?s) in (<slist>))"
+
+var filterLength = 5;
+
+var sQuery = "SELECT ?s WHERE { ?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence>} order by ?s"
+
+
 var reqBody = {
     "type": "eeaRDF",
     "eeaRDF" : {
         "endpoint" : "http://cr.eionet.europa.eu/sparql",
         "queryType" : "select",
-        "query" : ["SELECT ?s ?p ?o WHERE { ?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o <filter>}"],
+        "query" : [],
         "addLanguage" : false,
         "includeResourceURI" : true
     },
@@ -19,14 +35,11 @@ var reqBody = {
 var run = function(options, callbacks) {
     var SparqlClient = require('sparql-client');
     var endpoint = 'http://cr.eionet.europa.eu/sparql';
-    var query = "SELECT ?s WHERE { ?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence>} order by ?s limit 20"
     var client = new SparqlClient(endpoint);
-    var limit = 5;
 
-    var filtertemplate = "FILTER (str(?s) in (<slist>))"
     var reqQueryTemplate = reqBody.eeaRDF.query[0];
 
-    client.query(query)
+    client.query(sQuery)
         .execute(function(error, results){
             var slist = "";
             var step = 0;
@@ -36,17 +49,14 @@ var run = function(options, callbacks) {
                 }
                 slist = slist + '"' + results.results.bindings[i].s.value +'"'
                 step++;
-                if (step === limit){
-                    var filter = filtertemplate.split("<slist>").join(slist);
-                    reqBody.eeaRDF.query[0] = reqQueryTemplate.split("<filter>").join(filter);
-
-                    api.esAPI(options)
-                        .PUT('/_river/aqstats/_meta', reqBody, callbacks).execute();
+                if (step === filterLength){
+                    reqBody.eeaRDF.query.push(queryTemplate.split("<filter>").join(filterTemplate.split("<slist>").join(slist)));
                     step = 0;
                     slist = "";
-
                 }
             }
+            api.esAPI(options)
+                .PUT('/_river/aqstats/_meta', reqBody, callbacks).execute();
 
         });
 
