6e735e2efd60cd42eb99862679ad9d06c5f6d2fe
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-02-10 18:07:28 +0200

    Refs #21931; Started updated indexing query

--- webapps/aqstats/scripts/createIndex.js
+++ webapps/aqstats/scripts/createIndex.js
@@ -1,12 +1,92 @@
 var api = require('../../common/lib/esAPI.js');
 
 var queryTemplate =
-"SELECT ?s ?p ?o WHERE\
+"SELECT ?s if(bound(?pr), ?pr, ?p) as ?p if (bound(?isdate), year(?o), if (bound(?isnumber), round(?o*100)/100, if (bound(?iscoord), round(?o*10000)/10000, ?o))) as ?o WHERE\
     {\
-        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o FILTER(isLiteral(?o))}\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o FILTER(isLiteral(?o))\
+        }\
         UNION\
         {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
-            ?o1 <http://www.w3.org/2000/01/rdf-schema#label> ?o  }\
+            ?o1 <http://www.w3.org/2000/01/rdf-schema#label> ?o\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://dd.eionet.europa.eu/property/broaderMetric> ?o2 .\
+            ?o2 <http://www.w3.org/2000/01/rdf-schema#label> ?o\
+            FILTER (str(?p) in ('http://reference.eionet.europa.eu/aq/ontology/aggregationType'))\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/aggregationType.reportingmetric> as ?pr WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o .\
+            FILTER (str(?p) in ('http://reference.eionet.europa.eu/aq/ontology/beginPosition'))\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/beginPosition.year> as ?pr WHERE { } }\
+            { SELECT "true" as ?isdate WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o .\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/airqualityValue')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/airqualityValue.aqvalue> as ?pr WHERE { } }\
+            { SELECT "true" as ?isnumber WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o .\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/datacapturePct')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/datacapturePct.datacapture> as ?pr WHERE { } }\
+            { SELECT "true" as ?isnumber WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o .\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/station_lat')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/station_lat.stationlatitude> as ?pr WHERE { } }\
+            { SELECT "true" as ?iscoord WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o .\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/station_lon')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/station_lon.stationlongitude> as ?pr WHERE { } }\
+            { SELECT "true" as ?iscoord WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://reference.eionet.europa.eu/aq/ontology/relevantEmissions> ?o2 .\
+            ?o2 <http://reference.eionet.europa.eu/aq/ontology/stationClassification> ?o3 .\
+            ?o3 <http://www.w3.org/2004/02/skos/core#notation> ?o\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/samplingPoint')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/samplingPoint.stationtype> as ?pr WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://reference.eionet.europa.eu/aq/ontology/areaClassification> ?o2 .\
+            ?o2 <http://www.w3.org/2004/02/skos/core#notation> ?o\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/station')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/station.stationarea> as ?pr WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://reference.eionet.europa.eu/aq/ontology/analyticalTechnique> ?o2 .\
+            ?o2 <http://www.w3.org/2000/01/rdf-schema#label> ?o\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/procedure')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/procedure.analyticaltechnique> as ?pr WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://reference.eionet.europa.eu/aq/ontology/measurementType> ?o2 .\
+            ?o2 <http://www.w3.org/2000/01/rdf-schema#label> ?o\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/procedure')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/procedure.measurementtype> as ?pr WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://reference.eionet.europa.eu/aq/ontology/inletHeight> ?o\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/sample')\
+            {SELECT <http://reference.eionet.europa.eu/aq/ontology/sample.inletHeight> as ?pr WHERE { } }\
+        }\
+        UNION\
+        {?s a <http://reference.eionet.europa.eu/aq/ontology/ValidatedExceedence> ; ?p ?o1 .\
+            ?o1 <http://reference.eionet.europa.eu/aq/ontology/kerbDistance> ?o\
+            FILTER (str(?p) = 'http://reference.eionet.europa.eu/aq/ontology/sample')\
+            { SELECT <http://reference.eionet.europa.eu/aq/ontology/sample.kerbdistance> as ?pr WHERE { } }\
+        }\
         <filter>\
     }";
 
